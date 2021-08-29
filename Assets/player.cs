26fc48e56b0e4294367155eeffc3dbfa78d73881
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-08-29 15:32:04 +0900

    これもうまくいっていないので直します

---
+++ Assets/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("プレイヤーのスピード")] public float speed = 2.0f;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //ゲームオーバー
+        Destroy(gameObject);
+    }
+}

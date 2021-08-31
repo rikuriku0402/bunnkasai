15b041275b0077646037119085eb32849bd7e749
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-08-31 14:29:48 +0900

    針のスクリプト

---
+++ Assets/Scenes/Scrips/GameOver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text textGameOver;
+    private bool inGame;
+    void Start()
+    {
+        textGameOver.enabled = false;
+
+
+
+        inGame = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inGame)
+        {
+            GameObject PlayerObj = GameObject.Find("Player");
+            if ( PlayerObj== null)
+            {
+                textGameOver.enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Scrips/hari1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hari1 : MonoBehaviour
+{
+    float y = 10.0f;
+    void Update()
+    {
+        y -= 8.0f * Time.deltaTime;
+
+        if (y < 0.0f)
+        {
+            y = 10.0f;
+        }
+        transform.localPosition = new Vector3(0, y, 0);
+    }
+}

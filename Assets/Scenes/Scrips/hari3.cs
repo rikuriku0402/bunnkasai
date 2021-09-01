2ee963b663469bc14d7b7302d3caf158cee95af2
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-09-01 17:37:51 +0900

    Revert "バグを直しました"
    This reverts commit 0013ff3c042e10832a74c7ae0e2e18e274f45664.

---
+++ Assets/Scenes/Scrips/hari3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hari3 : MonoBehaviour
+{
+    float y = 14.0f;
+    void Update()
+    {
+        y -= 12.0f * Time.deltaTime;
+
+        if (y < 0.0f)
+        {
+            y = 14.0f;
+        }
+        transform.localPosition = new Vector3(4, y, 0);
+    }
+}
